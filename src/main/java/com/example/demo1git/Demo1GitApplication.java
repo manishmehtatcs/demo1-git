c30910c8fb1f6b47fb70503b5afb7c647badede7
manish <manish.1738108@gmail.com>
2020-03-31 13:09:30 +0530

    first commit

---
+++ src/main/java/com/example/demo1git/Demo1GitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1GitApplication.class, args);
+	}
+
+}
